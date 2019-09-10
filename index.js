fb141319d4920022b382b37ba6bbae23ddbd304f
David <david@sauropodstudio.com>
2019-09-10 15:03:43 -0400

    Updated THREEJS

--- index.js
+++ index.js
@@ -1,170 +1,135 @@
-var THREE = require('three')
-
-module.exports = function(data, mesher, scaleFactor, three) {
-  return new Mesh(data, mesher, scaleFactor, three)
+var THREE, temporaryPosition, temporaryVector
+const { SSAOEffect, EffectComposer, EffectPass, RenderPass, NormalPass,BlendFunction } = require("postprocessing");
+module.exports = function(three, opts) {
+  temporaryPosition = new three.Vector3
+  temporaryVector = new three.Vector3
+  
+  return new View(three, opts)
 }
 
-module.exports.Mesh = Mesh
+function View(three, opts) {
+  THREE = three // three.js doesn't support multiple instances on a single page
+  this.fov = opts.fov || 60
+  this.width = opts.width || 512
+  this.height = opts.height || 512
+  this.aspectRatio = opts.aspectRatio || this.width/this.height
+  this.nearPlane = opts.nearPlane || 1
+  this.farPlane = opts.farPlane || 10000
+  this.skyColor = opts.skyColor || 0xBFD1E5
+  this.ortho = opts.ortho
+  this.camera = this.ortho?(new THREE.OrthographicCamera(this.width/-2, this.width/2, this.height/2, this.height/-2, this.nearPlane, this.farPlane)):(new THREE.PerspectiveCamera(this.fov, this.aspectRatio, this.nearPlane, this.farPlane))
+  this.camera.lookAt(new THREE.Vector3(0, 0, 0))
+  this.clock = new THREE.Clock();
+  if (!process.browser) return
 
-function Mesh(data, mesher, scaleFactor, three) {
-  this.THREE = three || THREE
-  this.data = data
-  var geometry = this.geometry = new this.THREE.Geometry()
-  this.scale = scaleFactor || new this.THREE.Vector3(10, 10, 10)
-  
-  var result = mesher( data.voxels, data.dims )
-  this.meshed = result
-
-  geometry.vertices.length = 0
-  geometry.faces.length = 0
-
-  for (var i = 0; i < result.vertices.length; ++i) {
-    var q = result.vertices[i]
-    geometry.vertices.push(new this.THREE.Vector3(q[0], q[1], q[2]))
-  } 
-  
-  for (var i = 0; i < result.faces.length; ++i) {
-    geometry.faceVertexUvs[0].push(this.faceVertexUv(i))
-
-    var q = result.faces[i]
-      geometry.faceVertexUvs[0].push(this.faceVertexUv(i))
-      var f = new this.THREE.Face3(q[0], q[1], q[2])
-      var f2 = new this.THREE.Face3(q[2], q[3], q[0])
-       f.color = new this.THREE.Color(q[4])
-       f2.color = new this.THREE.Color(q[4])
-      geometry.faces.push(f2)
-      geometry.faces.push(f)
-
-  }
-  
-  geometry.computeFaceNormals()
-
-  // compute vertex colors for ambient occlusion
-  var light = new THREE.Color(0xffffff)
-  var shadow = new THREE.Color(0x505050)
-  for (var i = 0; i < geometry.faces.length; ++i) {
-    var face = geometry.faces[i]
-    // facing up
-    if (face.normal.y === 1)       face.vertexColors = [light, light, light, light]
-    // facing down
-    else if (face.normal.y === -1) face.vertexColors = [shadow, shadow, shadow, shadow]
-    // facing right
-    else if (face.normal.x === 1)  face.vertexColors = [shadow, light, light, shadow]
-    // facing left
-    else if (face.normal.x === -1) face.vertexColors = [shadow, shadow, light, light]
-    // facing backward
-    else if (face.normal.z === 1)  face.vertexColors = [shadow, shadow, light, light]
-    // facing forward
-    else                           face.vertexColors = [shadow, light, light, shadow]
-  }
-
-  geometry.verticesNeedUpdate = true
-  geometry.elementsNeedUpdate = true
-  geometry.normalsNeedUpdate = true
-
-  geometry.computeBoundingBox()
-  geometry.computeBoundingSphere()
-
+  this.createRenderer(opts.canvas)
+  this.element = this.renderer.domElement
 }
 
-Mesh.prototype.createWireMesh = function(hexColor) {    
-  var wireMaterial = new this.THREE.MeshBasicMaterial({
-    color : hexColor || 0xffffff,
-    wireframe : true
+View.prototype.createRenderer = function(canvas) {
+  this.renderer = new THREE.WebGLRenderer({
+    antialias: true,
+      canvas: canvas,
+    logarithmicDepthBuffer:true
   })
-  wireMesh = new THREE.Mesh(this.geometry, wireMaterial)
-  wireMesh.scale = this.scale
-  wireMesh.doubleSided = true
+  this.renderer.shadowMap.enabled = true
+  this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  this.renderer.setClearColor(this.skyColor, 1.0)
 
-  this.wireMesh = wireMesh
-  return wireMesh
+  this.effectComposer = new EffectComposer(this.renderer);
+
+
+ // this.saoPass = new SAOPass(this.scene, this.camera, false, true, new THREE.Vector2(window.innerWidth, window.innerHeight))
+
+  this.effectComposer.setSize(this.width, this.height)
+  this.renderer.clear()
 }
 
-Mesh.prototype.createSurfaceMesh = function(material) {
-  material = material || new this.THREE.MeshNormalMaterial()
-  var surfaceMesh  = new this.THREE.Mesh( this.geometry, material )
-  surfaceMesh.scale = this.scale
-  surfaceMesh.doubleSided = false
-  surfaceMesh.castShadow = true;
-  surfaceMesh.receiveShadow = true;
-  this.surfaceMesh = surfaceMesh
-  return surfaceMesh
+View.prototype.bindToScene = function(scene) {
+  scene.add(this.camera)
+  this.normalPass = new NormalPass(scene,this.camera);
+  this.renderPass = new RenderPass(scene, this.camera);
+  const ssaoEffect =new SSAOEffect(this.camera,this.normalPass.renderTarget.texture, {
+    blendFunction: BlendFunction.MULTIPLY,
+    samples: 32,
+    rings: 10,
+    distanceThreshold: 0.25,
+    distanceFalloff: 0.5,
+    rangeThreshold: 0.0015,
+    rangeFalloff: 0.01,
+    luminanceInfluence: 0.3,
+    radius:15,
+    scale: 0.35,
+    bias: 1.1
+  });
+  this.effectPass = new EffectPass(this.camera,ssaoEffect );
+  this.renderPass.renderToScreen = false;
+  this.effectPass.renderToScreen = true;
+ // this.renderPass.renderToScreen = true;
+  this.effectComposer.reset();
+  this.effectComposer.addPass(this.renderPass)
+  this.effectComposer.addPass(this.normalPass)
+  this.effectComposer.addPass(this.effectPass)
+
+  // var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+  // light.castShadow = true;
+  // scene.add( light );
+  var light = new THREE.AmbientLight(0xffe2b0,1.2); // soft white light
+  scene.add(light);
+  var light2 = new THREE.PointLight(0xffffff,0.3); // soft white light
+  light2.position.set( 30, 50, -16 );
+  light2.castShadow = true;
+  light2.shadow.mapSize.width = 2048;
+  light2.shadow.mapSize.height = 2048;
+  //light2.shadow.bias =0.0004;
+
+  scene.add(light2);
 }
 
-Mesh.prototype.addToScene = function(scene) {
-  if (this.wireMesh) scene.add( this.wireMesh )
-  if (this.surfaceMesh) scene.add( this.surfaceMesh )
+View.prototype.getCamera = function() {
+  return this.camera
 }
 
-Mesh.prototype.setPosition = function(x, y, z) {
-  if (this.wireMesh) this.wireMesh.position = new this.THREE.Vector3(x, y, z)
-  if (this.surfaceMesh) this.surfaceMesh.position = new this.THREE.Vector3(x, y, z)
+View.prototype.cameraPosition = function() {
+  temporaryPosition.multiplyScalar(0)
+  temporaryPosition.applyMatrix4(this.camera.matrixWorld)
+  return [temporaryPosition.x, temporaryPosition.y, temporaryPosition.z]
 }
 
-Mesh.prototype.faceVertexUv = function(i) {
-  var vs = [
-    this.meshed.vertices[i*4+0],
-    this.meshed.vertices[i*4+1],
-    this.meshed.vertices[i*4+2],
-    this.meshed.vertices[i*4+3]
-  ]
-  var spans = {
-    x0: vs[0][0] - vs[1][0],
-    x1: vs[1][0] - vs[2][0],
-    y0: vs[0][1] - vs[1][1],
-    y1: vs[1][1] - vs[2][1],
-    z0: vs[0][2] - vs[1][2],
-    z1: vs[1][2] - vs[2][2]
-  }
-  var size = {
-    x: Math.max(Math.abs(spans.x0), Math.abs(spans.x1)),
-    y: Math.max(Math.abs(spans.y0), Math.abs(spans.y1)),
-    z: Math.max(Math.abs(spans.z0), Math.abs(spans.z1))
-  }
-  if (size.x === 0) {
-    if (spans.y0 > spans.y1) {
-      var width = size.y
-      var height = size.z
-    }
-    else {
-      var width = size.z
-      var height = size.y
-    }
-  }
-  if (size.y === 0) {
-    if (spans.x0 > spans.x1) {
-      var width = size.x
-      var height = size.z
-    }
-    else {
-      var width = size.z
-      var height = size.x
-    }
-  }
-  if (size.z === 0) {
-    if (spans.x0 > spans.x1) {
-      var width = size.x
-      var height = size.y
-    }
-    else {
-      var width = size.y
-      var height = size.x
-    }
-  }
-  if ((size.z === 0 && spans.x0 < spans.x1) || (size.x === 0 && spans.y0 > spans.y1)) {
-    return [
-      new this.THREE.Vector2(height, 0),
-      new this.THREE.Vector2(0, 0),
-      new this.THREE.Vector2(0, width),
-      new this.THREE.Vector2(height, width)
-    ]
-  } else {
-    return [
-      new this.THREE.Vector2(0, 0),
-      new this.THREE.Vector2(0, height),
-      new this.THREE.Vector2(width, height),
-      new this.THREE.Vector2(width, 0)
-    ]
-  }
+View.prototype.cameraVector = function() {
+  temporaryVector.multiplyScalar(0)
+  temporaryVector.z = -1
+  temporaryVector.transformDirection( this.camera.matrixWorld )
+  return [temporaryVector.x, temporaryVector.y, temporaryVector.z]
 }
-;
+
+View.prototype.resizeWindow = function(width, height) {
+  if (this.element.parentElement) {
+    width = width || this.element.parentElement.clientWidth
+    height = height || this.element.parentElement.clientHeight
+  }
+
+  this.camera.aspect = this.aspectRatio = width/height
+  this.width = width
+  this.height = height
+
+  this.camera.updateProjectionMatrix()
+
+  this.effectComposer.setSize( width, height )
+}
+
+View.prototype.render = function(scene) {
+  this.effectComposer.render(this.clock.getDelta())
+ // this.renderer.render(scene, this.camera)
+}
+
+View.prototype.appendTo = function(element) {
+  if (typeof element === 'object') {
+    element.appendChild(this.element)
+  }
+  else {
+    document.querySelector(element).appendChild(this.element)
+  }
+
+  this.resizeWindow(this.width,this.height)
+}
