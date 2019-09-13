53599884ee1e14527974b351ab472daa93f3acc7
David <david@sauropodstudio.com>
2019-09-13 11:29:50 -0400

    Moved sprites to another composer. Disabled SSAO until depth bug is fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var THREE, temporaryPosition, temporaryVector
-const { SSAOEffect, EffectComposer, EffectPass, RenderPass, NormalPass,BlendFunction } = require("postprocessing");
+const { DepthPass,SSAOEffect, EffectComposer, EffectPass, RenderPass, NormalPass,BlendFunction,MaskPass, ClearMaskPass,ClearPass } = require("postprocessing");
 module.exports = function(three, opts) {
   temporaryPosition = new three.Vector3
   temporaryVector = new three.Vector3
@@ -24,6 +24,7 @@ function View(three, opts) {
 
   this.createRenderer(opts.canvas)
   this.element = this.renderer.domElement
+  this.effectComposers = [];
 }
 
 View.prototype.createRenderer = function(canvas) {
@@ -33,47 +34,75 @@ View.prototype.createRenderer = function(canvas) {
     logarithmicDepthBuffer:true
   })
   this.renderer.shadowMap.enabled = true
+  this.renderer.autoClear = false;
   this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   this.renderer.setClearColor(this.skyColor, 1.0)
 
-  this.effectComposer = new EffectComposer(this.renderer);
-
-
- // this.saoPass = new SAOPass(this.scene, this.camera, false, true, new THREE.Vector2(window.innerWidth, window.innerHeight))
-
-  this.effectComposer.setSize(this.width, this.height)
-  this.renderer.clear()
 }
 
-View.prototype.bindToScene = function(scene) {
-  scene.add(this.camera)
-  this.normalPass = new NormalPass(scene,this.camera);
-  this.renderPass = new RenderPass(scene, this.camera);
-  const ssaoEffect =new SSAOEffect(this.camera,this.normalPass.renderTarget.texture, {
-    blendFunction: BlendFunction.MULTIPLY,
-    samples: 32,
-    rings: 10,
-    distanceThreshold: 0.25,
-    distanceFalloff: 0.5,
-    rangeThreshold: 0.0015,
-    rangeFalloff: 0.01,
-    luminanceInfluence: 0.3,
-    radius:15,
-    scale: 0.35,
-    bias: 1.1
-  });
-  this.effectPass = new EffectPass(this.camera,ssaoEffect );
-  this.renderPass.renderToScreen = false;
-  this.effectPass.renderToScreen = true;
- // this.renderPass.renderToScreen = true;
-  this.effectComposer.reset();
-  this.effectComposer.addPass(this.renderPass)
-  this.effectComposer.addPass(this.normalPass)
-  this.effectComposer.addPass(this.effectPass)
+View.prototype.bindToScene = function(scene,applyEffectPass) {
 
-  // var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-  // light.castShadow = true;
-  // scene.add( light );
+  if (!this.scene)
+  {
+    this.scene = scene;
+  }
+
+  let composer = new EffectComposer(this.renderer);
+  composer.autoClear = false;
+  this.effectComposers.push(composer);
+  composer.setSize(this.width, this.height)
+  this.renderer.clear();
+
+  scene.add(this.camera);
+  let normalPass = new NormalPass(scene,this.camera);
+  let renderPass = new RenderPass(scene, this.camera);
+
+  renderPass.clear = applyEffectPass;
+  renderPass.renderToScreen = false;
+  if (applyEffectPass)
+  {
+    const ssaoEffect =new SSAOEffect(this.camera,normalPass.renderTarget.texture, {
+      blendFunction: BlendFunction.MULTIPLY,
+      samples: 32,
+      rings: 10,
+      distanceThreshold: 0.25,
+      distanceFalloff: 0.5,
+      rangeThreshold: 0.0015,
+      rangeFalloff: 0.01,
+      luminanceInfluence: 0.3,
+      radius:15,
+      scale: 0.35,
+      bias: 1.1
+    });
+    let effectPass = new EffectPass(this.camera,ssaoEffect );
+    renderPass.renderToScreen = true;
+  //  effectPass.renderToScreen = true;
+   // effectPass.clear = true;
+   // normalPass.clear = true;
+    let clearPass = new ClearPass();
+   // composer.addPass(clearPass);
+    composer.createDepthTexture();
+    composer.addPass(normalPass);
+    composer.addPass(renderPass);
+    composer.addPass(effectPass);
+
+  }
+  else {
+   // let depthPass = new DepthPass(this.scene,this.camera);
+    //composer.addPass(clearPass);
+    //  composer.addPass(this.maskPass);
+   // renderPassTerrain.clear = false;
+   //  renderPass.clear = false;
+   //  depthPass.clear = false;
+   // renderPass.setDepthTexture( this.effectComposers[0].createDepthTexture());
+   // composer.addPass(depthPass);
+    composer.addPass(renderPass);
+ //   depthPass.clear = false;
+   // depthPass.renderToScreen = true;
+    renderPass.renderToScreen = true;
+  }
+
+
   var light = new THREE.AmbientLight(0xffe2b0,1.2); // soft white light
   scene.add(light);
   var light2 = new THREE.PointLight(0xffffff,0.3); // soft white light
@@ -115,11 +144,48 @@ View.prototype.resizeWindow = function(width, height) {
 
   this.camera.updateProjectionMatrix()
 
-  this.effectComposer.setSize( width, height )
+  this.effectComposers.forEach(c=>c.setSize( width, height ));
 }
 
 View.prototype.render = function(scene) {
-  this.effectComposer.render(this.clock.getDelta())
+  this.renderer.clear();
+  this.effectComposers.forEach(c=>c.render(this.clock.getDelta()))
+
+  // for (let i = this.effectComposers.length-1; i>=0;i--)
+  // {
+  //   this.effectComposers[i].render(this.clock.getDelta());
+  //   let isMask = (i === 0);
+  //   if (isMask)
+  //   {
+  //     this.renderer.context.colorMask(true, true, true, true);
+  //     this.effectComposers[i].render(this.clock.getDelta());
+  //   }else {
+  //     this.renderer.context.colorMask(false, false, false, false);
+  //   }
+  // }
+  // this.renderer.context.colorMask(false, false, false, false);
+  // this.effectComposers[0].render(this.clock.getDelta());
+  //
+  // this.renderer.context.colorMask(true, true, true, true);
+  // this.effectComposers[0].render(this.clock.getDelta());
+  // this.effectComposers[1].render(this.clock.getDelta());
+
+  // for (let i = 0; i<this.effectComposers.length;i++)
+  // {
+  //   let isMask = (i === 0);
+  //   if (isMask)
+  //   {
+  //     this.renderer.context.colorMask(false, false, false, false);
+  //     this.effectComposers[i].render(this.clock.getDelta());
+  //     this.renderer.context.colorMask(true, true, true, true);
+  //     this.effectComposers[i].render(this.clock.getDelta());
+  //   }
+  //   else
+  //   {
+  //     this.effectComposers[i].render(this.clock.getDelta());
+  //   }
+  // }
+
  // this.renderer.render(scene, this.camera)
 }
 
